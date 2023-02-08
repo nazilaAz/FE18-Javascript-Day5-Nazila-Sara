9c2fbdde58577eda16d195f5c2e6d3e69e2ef397
Nazila <azabdaftar@outlook.com>
2023-02-08 15:06:23 +0100

    Animals added

--- ClassesBasic/EX 1/js/script.js
+++ ClassesBasic/EX 1/js/script.js
@@ -18,7 +18,7 @@ class Person {
 
 const psample1 = new Person("Hannah", "Puzt", 25, "45");
 
-document.getElementById("container").innerHTML +=
+document.getElementById("firstcol").innerHTML +=
     `
 <div class="card" style="width: 18rem;">
 <img src="img/Pic.png" class="card-img-top" alt="...">
@@ -32,6 +32,36 @@ document.getElementById("container").innerHTML +=
 </div> 
 `;
 
-class Animal {}
+class Animal {
+    aName;
+    aColor;
+    aSpeed;
+    aLegs;
+
+    constructor(animalName, animalColor, animalSpeed, animalLeg) {
+        this.aName = animalName;
+        this.aColor = animalColor;
+        this.aSpeed = animalSpeed;
+        this.aLegs = animalLeg;
+    }
+    animalInfos() {
+        return ` The ${this.aName} is ${this.aColor}, can ${this.aSpeed} speed and has ${this.aLegs} legs. `;
+    }
+}
+const sampleAnimal = new Animal("Horse", "Black", "60km", 4);
+
+document.getElementById("secondcol").innerHTML +=
+    `
+<div class="card" style="width: 18rem;">
+<img src="img/Pic.png" class="card-img-top" alt="...">
+<div class="card-body">
+<h5 class="card-title">${sampleAnimal.aName} -  ${sampleAnimal.aLegs}</h5>
+<h6 class="card-subtitle mb-2 text-muted">(${sampleAnimal.aColor})</h6>
+<p class="card-text">${sampleAnimal.animalInfos()}</p>
+<a href="#" class="card-link">Her/ His Information</a>
+<a href="#" class="card-link">Her / His Car</a>
+</div>
+</div> 
+`;
 
 class Car {}
